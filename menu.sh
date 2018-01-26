3eebb8d2ce51cef509a59f20b630a76c8202282d
rotemsg <35840276+rotemsg@users.noreply.github.com>
2018-01-26 15:31:09 +0200

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+
+param=$1
+last_cmd=0
+
+inst_data="some text for the top left"
+title="Select Action"
+
+function main(){
+
+    menu_options=()
+#    grep '^#func:' workers.stats.sh | sed 's/#func://' > workers.stats.menu
+    cat $0 | grep -A1 '^#func:' | grep -v '^-' | sed 's/(){//' | sed 's/^function //' | sed 's/^#func://' | while read l1; do read l2; echo "$l2 $l1"; done > workers.stats.menu
+
+    while read -r number text; do
+        menu_options+=( ${number//\"} "${text//\"}" )
+    done < workers.stats.menu
+
+    export inp=$(whiptail --backtitle "`date` $inst_data" --title $title  --cancel-button "Quit" --notags --default-item $last_cmd --menu "Choose an option"  25 78 16 "${menu_options[@]}" 3>&2 2>&1 1>&3-)
+
+    [ ! $inp ] && exit
+
+    last_cmd=$inp
+
+    $inp
+
+}
+
+
+
+#func: "action number 1"
+function any_func_name(){
+  echo "hello world"
+}
+
+
+#func: "action number 2"
+function another_func_name(){
+  echo "hello world 2"
+}
+
+
